ad546d95cccd85bf98658eb222eaba4296e47ed4
vrbaj <vrbik.vrbik@gmail.com>
2018-09-25 23:11:45 +0200

    tare and zero added

--- gui7.py
+++ gui7.py
@@ -24,13 +24,14 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtCore import QThread
 import pyqtgraph as pq
 
+from pyqtgraph import PlotWidget
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         pq.setConfigOption('background', 'y')
         pq.setConfigOption('foreground', 'k')
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(912, 659)
+        MainWindow.resize(912, 732)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.text_hodnoty1 = QtWidgets.QTextBrowser(self.centralwidget)
@@ -46,9 +47,6 @@ class Ui_MainWindow(object):
         font.setPointSize(14)
         font.setBold(True)
         font.setWeight(75)
-
-
-
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
@@ -207,20 +205,16 @@ class Ui_MainWindow(object):
         self.lineEdit = QtWidgets.QLineEdit(self.groupBox_3)
         self.lineEdit.setGeometry(QtCore.QRect(20, 100, 221, 20))
         self.lineEdit.setObjectName("lineEdit")
-        self.lineEdit.setText(datetime.datetime.now().strftime("Data_"+"%Y-%m-%d_%H%M%S"+".csv"))
+        self.lineEdit.setText(datetime.datetime.now().strftime("Data_" + "%Y-%m-%d_%H%M%S" + ".csv"))
         reg_ex = QtCore.QRegExp("([a-zA-Z0-9\s_,\]\[\(\)\-:])+.csv$")
-        self.lineEdit.setValidator(QtGui.QRegExpValidator(reg_ex, self.lineEdit))
-
         self.lineEdit_interval = QtWidgets.QLineEdit(self.groupBox_3)
         self.lineEdit_interval.setGeometry(QtCore.QRect(90, 60, 151, 20))
         self.lineEdit_interval.setInputMethodHints(QtCore.Qt.ImhDigitsOnly)
         self.lineEdit_interval.setObjectName("lineEdit_interval")
-        self.lineEdit_interval.setValidator(QtGui.QIntValidator())
         self.lineEdit_pocet_2 = QtWidgets.QLineEdit(self.groupBox_3)
         self.lineEdit_pocet_2.setGeometry(QtCore.QRect(90, 40, 151, 20))
         self.lineEdit_pocet_2.setInputMethodHints(QtCore.Qt.ImhDigitsOnly)
         self.lineEdit_pocet_2.setObjectName("lineEdit_pocet_2")
-        self.lineEdit_pocet_2.setValidator(QtGui.QIntValidator())
         self.buttonMereniDynamicke = QtWidgets.QPushButton(self.groupBox_3)
         self.buttonMereniDynamicke.setGeometry(QtCore.QRect(20, 120, 221, 21))
         self.buttonMereniDynamicke.setObjectName("buttonMereniDynamicke")
@@ -242,6 +236,46 @@ class Ui_MainWindow(object):
         self.label_13 = QtWidgets.QLabel(self.groupBox_3)
         self.label_13.setGeometry(QtCore.QRect(20, 150, 47, 13))
         self.label_13.setObjectName("label_13")
+        self.groupBox_4 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_4.setGeometry(QtCore.QRect(490, 660, 411, 51))
+        self.groupBox_4.setObjectName("groupBox_4")
+        self.pushTare2 = QtWidgets.QPushButton(self.groupBox_4)
+        self.pushTare2.setGeometry(QtCore.QRect(10, 20, 91, 23))
+        self.pushTare2.setObjectName("pushTare2")
+        self.pushTareZrusit2 = QtWidgets.QPushButton(self.groupBox_4)
+        self.pushTareZrusit2.setGeometry(QtCore.QRect(110, 20, 91, 23))
+        self.pushTareZrusit2.setObjectName("pushTareZrusit2")
+        self.pushNula2 = QtWidgets.QPushButton(self.groupBox_4)
+        self.pushNula2.setGeometry(QtCore.QRect(210, 20, 91, 23))
+        self.pushNula2.setObjectName("pushNula2")
+        self.pushNulaZrusit2 = QtWidgets.QPushButton(self.groupBox_4)
+        self.pushNulaZrusit2.setGeometry(QtCore.QRect(310, 20, 91, 23))
+        self.pushNulaZrusit2.setObjectName("pushNulaZrusit2")
+        self.groupBox_5 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_5.setGeometry(QtCore.QRect(10, 660, 411, 51))
+        self.groupBox_5.setObjectName("groupBox_5")
+        self.pushTare2_2 = QtWidgets.QPushButton(self.groupBox_5)
+        self.pushTare2_2.setGeometry(QtCore.QRect(10, 20, 91, 23))
+        self.pushTare2_2.setObjectName("pushTare2_2")
+        self.pushTareZrusit2_2 = QtWidgets.QPushButton(self.groupBox_5)
+        self.pushTareZrusit2_2.setGeometry(QtCore.QRect(110, 20, 91, 23))
+        self.pushTareZrusit2_2.setObjectName("pushTareZrusit2_2")
+        self.pushNula1 = QtWidgets.QPushButton(self.groupBox_5)
+        self.pushNula1.setGeometry(QtCore.QRect(210, 20, 91, 23))
+        self.pushNula1.setObjectName("pushNula1")
+        self.pushNulaZrusit1 = QtWidgets.QPushButton(self.groupBox_5)
+        self.pushNulaZrusit1.setGeometry(QtCore.QRect(310, 20, 91, 23))
+        self.pushNulaZrusit1.setObjectName("pushNulaZrusit1")
+
+        self.pushNula1.clicked.connect(self.nula1)
+        self.pushNula2.clicked.connect(self.nula2)
+        self.pushTare2.clicked.connect(self.tare1)
+        self.pushTare2_2.clicked.connect(self.tare2)
+        self.pushNulaZrusit1.clicked.connect(self.nula1_zrusit)
+        self.pushNulaZrusit2.clicked.connect(self.nula2_zrusit)
+        self.pushTareZrusit2.clicked.connect(self.tare1_zrusit)
+        self.pushTareZrusit2_2.clicked.connect(self.tare2_zrusit)
+
         self.groupBox.raise_()
         self.text_hodnoty1.raise_()
         self.text_hodnoty2.raise_()
@@ -258,6 +292,8 @@ class Ui_MainWindow(object):
         self.smodch2.raise_()
         self.groupBox_2.raise_()
         self.groupBox_3.raise_()
+        self.groupBox_4.raise_()
+        self.groupBox_5.raise_()
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -266,6 +302,59 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Měření"))
+        self.label.setText(_translate("MainWindow", "Statické měření"))
+        self.label_2.setText(_translate("MainWindow", "Čidlo 1"))
+        self.label_3.setText(_translate("MainWindow", "Čidlo 2"))
+        self.label_4.setText(_translate("MainWindow", "Průměrná hodnota"))
+        self.label_5.setText(_translate("MainWindow", "Směrodatná odchylka"))
+        self.label_6.setText(_translate("MainWindow", "Průměrná hodnota"))
+        self.label_7.setText(_translate("MainWindow", "Směrodatná odchylka"))
+        self.buttonMereni.setText(_translate("MainWindow", "Měření"))
+        self.label_8.setText(_translate("MainWindow", "Rozdíl"))
+        self.label_9.setText(_translate("MainWindow", "Dynamické měření"))
+        self.label_10.setText(_translate("MainWindow", "Počet měření"))
+        self.label_11.setText(_translate("MainWindow", "Interval [s]"))
+        self.label_12.setText(_translate("MainWindow", "Název souboru"))
+        self.buttonMereniDynamicke.setText(_translate("MainWindow", "Zahájit měření"))
+        self.label_13.setText(_translate("MainWindow", "Hodnoty"))
+        self.groupBox_4.setTitle(_translate("MainWindow", "Čidlo 2"))
+        self.pushTare2.setText(_translate("MainWindow", "Tare"))
+        self.pushTareZrusit2.setText(_translate("MainWindow", "Vynulovat Tare"))
+        self.pushNula2.setText(_translate("MainWindow", "Vynulovat"))
+        self.pushNulaZrusit2.setText(_translate("MainWindow", "Zrušit 0"))
+        self.groupBox_5.setTitle(_translate("MainWindow", "Čidlo 1"))
+        self.pushTare2_2.setText(_translate("MainWindow", "Tare"))
+        self.pushTareZrusit2_2.setText(_translate("MainWindow", "Vynulovat Tare"))
+        self.pushNula1.setText(_translate("MainWindow", "Vynulovat"))
+        self.pushNulaZrusit1.setText(_translate("MainWindow", "Zrušit 0"))
+
+    def nula1(self):
+        # {"id":27,"method":"call","params":{"path":"measval/cmdSetZeroGros","args":[]}}
+        self.send_command(1, "zero")
+
+    def nula2(self):
+        self.send_command(1, "zero")
+
+    def tare2(self):
+        self.send_command(1, "tare")
+
+    def tare1(self):
+        self.send_command(1, "tare")
+
+    def nula1_zrusit(self):
+        self.send_command(1, "clear_zero")
+
+    def nula2_zrusit(self):
+        self.send_command(1, "clear_zero")
+
+    def tare1_zrusit(self):
+        self.send_command(1, "clear_tare")
+
+    def tare2_zrusit(self):
+        self.send_command(1, "clear_tare")
 
     def multiple_measure(self):
         print("multiple")
@@ -292,18 +381,18 @@ class Ui_MainWindow(object):
             self.smodch1.setText("0")
             self.smodch2.setText("0")
             json_trigger_command = """
-                                {"id":1,"method":"call","params":{"path":"measval/cmdTriggerCapturedValue1","args":[]}}
-                                """
+                                   {"id":1,"method":"call","params":{"path":"measval/cmdTriggerCapturedValue1","args":[]}}
+                                   """
             json_get_command = """
-                                {"method":"fetch_all","params":{"path":"measval/values/capturedValue1"}}
-                                """
+                                   {"method":"fetch_all","params":{"path":"measval/values/capturedValue1"}}
+                                   """
             json_trigger = json.loads(json_trigger_command)
             json_get = json.loads(json_get_command)
             expected_message = """"path":"measval/values/capturedValue1"""""
             sensor1_values = [None] * 10
             measured_values = np.zeros((10,))
             try:
-                ws = create_connection("ws://10.0.0.46:8081")
+                ws = create_connection("ws://10.0.0.52:8081")
             except Exception as ex:
                 print(ex)
             print("Start mereni 1:" + str(datetime.datetime.now()))
@@ -364,7 +453,7 @@ class Ui_MainWindow(object):
             if len(dyn_value) > 1:
                 try:
                     self.graphicsView.plot(dyn_value, pen=pq.mkPen('b', width=3,
-                                                                         style=QtCore.Qt.SolidLine, color=(200, 200, 255)))
+                                                                   style=QtCore.Qt.SolidLine, color=(200, 200, 255)))
                     self.graphicsView.getPlotItem().showGrid(x=True, y=True, alpha=1)
                     QtGui.QGuiApplication.processEvents()
                 except Exception as ex:
@@ -383,13 +472,12 @@ class Ui_MainWindow(object):
         QtGui.QGuiApplication.processEvents()
         output = np.asarray([dyn_value, dyn_value, dyn_value, dyn_value, dyn_value])
         try:
-            np.savetxt(self.lineEdit.text(), output.transpose(), delimiter=",", header="diff,smodch1,smodch2,prumer1,prumer2")
+            np.savetxt(self.lineEdit.text(), output.transpose(), delimiter=",",
+                       header="diff,smodch1,smodch2,prumer1,prumer2")
         except Exception as ex:
             print(ex)
         self.lineEdit.setText(datetime.datetime.now().strftime("Data_" + "%Y-%m-%d_%H%M%S" + ".csv"))
         QtGui.QGuiApplication.processEvents()
-
-
 
     def one_measure(self):
         self.buttonMereni.setEnabled(False)
@@ -402,18 +490,18 @@ class Ui_MainWindow(object):
         self.prumer2.setText("")
         self.rozdil.setText("")
         json_trigger_command = """
-                    {"id":1,"method":"call","params":{"path":"measval/cmdTriggerCapturedValue1","args":[]}}
-                    """
+                       {"id":1,"method":"call","params":{"path":"measval/cmdTriggerCapturedValue1","args":[]}}
+                       """
         json_get_command = """
-                    {"method":"fetch_all","params":{"path":"measval/adcBinVal32"}}
-                    """
+                       {"method":"fetch_all","params":{"path":"measval/adcBinVal32"}}
+                       """
         json_trigger = json.loads(json_trigger_command)
         json_get = json.loads(json_get_command)
         expected_message = """"path":"measval/values/capturedValue1"""""
         sensor1_values = [None] * 10
         measured_values = np.zeros((10,))
 
-        ws = create_connection("ws://10.0.0.46:8081")
+        ws = create_connection("ws://10.0.0.52:8081")
 
         for mereni in range(10):
             ws.send(json.dumps(json_trigger))
@@ -436,7 +524,7 @@ class Ui_MainWindow(object):
         QtGui.QGuiApplication.processEvents()
         sensor2_values = [None] * 10
         measured2_values = np.zeros((10, 1))
-        ws2 = create_connection("ws://10.0.0.46:8081")
+        ws2 = create_connection("ws://10.0.0.52:8081")
         self.text_hodnoty2.setText("")
         for mereni in range(10):
             ws2.send(json.dumps(json_trigger))
@@ -466,27 +554,41 @@ class Ui_MainWindow(object):
         self.buttonMereniDynamicke.setEnabled(True)
         QtGui.QGuiApplication.processEvents()
 
+    def send_command(self, device, command):
+        if device == 1:
+            connection_string = "ws://10.0.0.52:8081"
+        else:
+            connection_string = "ws://10.0.0.46:8081"
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Měření"))
-        self.label.setText(_translate("MainWindow", "Statické měření"))
-        self.label_2.setText(_translate("MainWindow", "Čidlo 1"))
-        self.label_3.setText(_translate("MainWindow", "Čidlo 2"))
-        self.label_4.setText(_translate("MainWindow", "Průměrná hodnota"))
-        self.label_5.setText(_translate("MainWindow", "Směrodatná odchylka"))
-        self.label_6.setText(_translate("MainWindow", "Průměrná hodnota"))
-        self.label_7.setText(_translate("MainWindow", "Směrodatná odchylka"))
-        self.buttonMereni.setText(_translate("MainWindow", "Měření"))
-        self.label_8.setText(_translate("MainWindow", "Rozdíl"))
-        self.label_9.setText(_translate("MainWindow", "Dynamické měření"))
-        self.label_10.setText(_translate("MainWindow", "Počet měření"))
-        self.label_11.setText(_translate("MainWindow", "Interval [s]"))
-        self.label_12.setText(_translate("MainWindow", "Název souboru"))
-        self.buttonMereniDynamicke.setText(_translate("MainWindow", "Zahájit měření"))
-        self.label_13.setText(_translate("MainWindow", "Hodnoty"))
+        if command == "tare":
+            json_command = """
+            {"id":2,"method":"call","params":{"path":"measval/cmdSetZeroNet","args":[]}}"""
+        elif command == "clear_tare":
+            json_command = """
+            {"id":31,"method":"call","params":{"path":"measval/cmdClearOffsetNet","args":[]}}"""
+        elif command == "zero":
+            json_command = """
+            {"id":32,"method":"call","params":{"path":"measval/cmdSetZeroGros","args":[]}}"""
+            # json_command = """
+            # {"id":68,"method":"set","params":{"path":"measval/scaling/zeroTargetGros","value":150}}"""
+        elif command == "clear_zero":
+            json_command = """
+            {"id":33,"method":"call","params":{"path":"measval/cmdClearOffsetGros","args":[]}}"""
+        else:
+            print("ERROR")
+            json_command = """"""
+        try:
+            ws = create_connection(connection_string)
+        except Exception as ex:
+            print(ex)
 
-from pyqtgraph import PlotWidget
+        json_tare = json.loads(json_command)
+        ws.send(json.dumps(json_tare))
+        result = ws.recv()
+        print(result)
+        ws.close()
+
+
 
 if __name__ == "__main__":
     import sys
@@ -496,4 +598,3 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
